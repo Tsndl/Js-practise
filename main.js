46d54f75b6baedfc7d3a1de2c693a86623fadf22
Tsndl <125467540+Tsndl@users.noreply.github.com>
2023-02-17 15:24:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const ul = document.querySelector('.items');
 //ul.lastElementChild.remove();
 ul. firstElementChild.textContent = 'Kowalski';
 ul.children[2].innerText = 'Tsndl';
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', (e) => {
+  console.log('click');
+});
