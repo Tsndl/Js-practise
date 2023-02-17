c28e95370d791824ac7bbb3107c698b42f6630bd
Tsndl <tsndljr@gmail.com>
2023-02-17 16:03:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ const btn = document.querySelector('.btn');
 btn.addEventListener('click', (e) => {
   console.log('click');
 });
+
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+}
